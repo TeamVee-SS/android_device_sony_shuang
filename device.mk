d3cd51e71210e780ed79eb11a14aaf5265846177
Michael Bestas <mikeioannina@gmail.com>
2015-04-01 01:59:56 +0300

    msm7x30-common: Use ART small mode
    https://source.android.com/devices/tech/dalvik/configure.html#with_art_small_mode
    
    Change-Id: Icdd631f997b5569783d121547796515e884dc22e

--- device.mk
+++ device.mk
@@ -346,7 +346,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-swap="false"
+    dalvik.vm.dex2oat-filter="interpret-only" \
+    dalvik.vm.dex2oat-swap="false" \
+    dalvik.vm.image-dex2oat-filter="speed"
 
 # Low Memory Props
 PRODUCT_PROPERTY_OVERRIDES += \
