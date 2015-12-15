961693accd6bd6d376296767421673b5aa81191d
Dan Willemsen <dwillemsen@google.com>
2015-12-14 16:29:36 -0800

    shuang: Remove TARGET_GLOBAL_CFLAGS use
    This doesn't do anything - TARGET_GLOBAL_CFLAGS is reset in
    build/core/combo/select.mk. (And these options are already in the global
    cflags for ARMv7 targets).
    
    BoardConfig files also should not be changing global cflags.
    
    Change-Id: Ie6944a8e14dd7aeb17a0c8f241511bc05a26d851

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,8 +33,6 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8610
