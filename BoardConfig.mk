9e19a790c01f3457ba5e7f9fdc1444a7b1ce10e9
Ethan Chen <intervigil@gmail.com>
2016-02-09 01:24:25 -0800

    shuang: Remove deprecated flag
    * EXTENDED_FONT_FOOTPRINT is deprecated
    
    Change-Id: I92784d83834fb4da52ae96ba1f495694b68f2f74

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,9 +178,6 @@ TW_IGNORE_OVERLAY := true
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/sony/falconss/sepolicy
 
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
-
 BOARD_RIL_CLASS := ../../../device/sony/falconss/ril/
 
 # Build the platform with Clang
