08b99d7de313ca91b968cda87b376a93a6602720
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    shuang: Allow vendor init to override props
    Change-Id: Ic7c4d86616bfe33b9668b5b54d119d0380f4660c

--- init/init_shuang.cpp
+++ init/init_shuang.cpp
@@ -33,6 +33,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
 #include "log.h"
 #include "property_service.h"
 #include "util.h"
@@ -41,6 +44,17 @@
 #define LTALABEL "/lta-label"
 
 enum { D2004, D2005, D2104, D2105, D2114, UNKNOWN };
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 static int model_number_from_ltalabel() {
     DIR* dir;
