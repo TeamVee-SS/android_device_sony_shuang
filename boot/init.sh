203b66e839b2148b352367a661c59bbd4557e052
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-03-12 23:14:28 -0300

    init: Move framebuffer alignment fix to recovery init
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- boot/init.sh
+++ boot/init.sh
@@ -51,7 +51,6 @@ load_image=/sbin/ramdisk.cpio
 
 # boot decision
 if [ -s /dev/keycheck ] || busybox grep -q warmboot=0x77665502 /proc/cmdline; then
-	busybox echo 0 > /sys/module/msm_fb/parameters/align_buffer
 	busybox echo 'RECOVERY BOOT' >> boot.txt
 	# trigger ON cyan LED for recoveryboot
 	triggerledrgb 0 255 255
