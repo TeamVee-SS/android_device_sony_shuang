8fe931fe7e9f47f35ebb791308a5d3b70c0f42cc
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-03-18 08:23:26 -0300

    init.qcom.wifi.sh: Reduce script
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- rootdir/root/init.qcom.wifi.sh
+++ rootdir/root/init.qcom.wifi.sh
@@ -26,94 +26,75 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-target="$1"
-serialno="$2"
-
 # No path is set up at this point so we have to do it here.
 PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 export PATH
 
-if [ ! -f /data/misc/wifi/wifi_fixup ];then
-	# This should be the first command
-	# remount system as read-write.
-	mount -o rw,remount,barrier=1 /system
-
-	case "$target" in
-	msm8610*)
-		# Check whether device is plugged on the HSIC bus
-		# Currently HSIC bus will be the first index
-		if [ -e /sys/bus/platform/drivers/msm_hsic_host ]; then
-			if [ ! -L /sys/bus/usb/devices/1-1 ]; then
-				echo msm_hsic_host > /sys/bus/platform/drivers/msm_hsic_host/unbind
-			fi
-			chown -h system.system /sys/bus/platform/drivers/msm_hsic_host/bind
-			chown -h system.system /sys/bus/platform/drivers/msm_hsic_host/unbind
-			chmod -h 0200 /sys/bus/platform/drivers/msm_hsic_host/bind
-			chmod -h 0200 /sys/bus/platform/drivers/msm_hsic_host/unbind
+case `getprop ro.board.platform` in
+msm8610*)
+	# Check whether device is plugged on the HSIC bus
+	# Currently HSIC bus will be the first index
+	if [ -e /sys/bus/platform/drivers/msm_hsic_host ]; then
+		if [ ! -L /sys/bus/usb/devices/1-1 ]; then
+			echo msm_hsic_host > /sys/bus/platform/drivers/msm_hsic_host/unbind
 		fi
+		chown -h system.system /sys/bus/platform/drivers/msm_hsic_host/bind
+		chown -h system.system /sys/bus/platform/drivers/msm_hsic_host/unbind
+		chmod -h 0200 /sys/bus/platform/drivers/msm_hsic_host/bind
+		chmod -h 0200 /sys/bus/platform/drivers/msm_hsic_host/unbind
+	fi
+	echo "*** WI-FI chip ID is not specified in /persist/wlan_chip_id **"
+	echo "*** Use the default WCN driver.                             **"
+	setprop wlan.driver.ath 0
 
-		echo "*** WI-FI chip ID is not specified in /persist/wlan_chip_id **"
-		echo "*** Use the default WCN driver.                             **"
-		setprop wlan.driver.ath 0
+	rm /data/misc/wifi/WCNSS_qcom_cfg.ini
+	rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
+	cp /system/etc/wifi/WCNSS_qcom_wlan_nv.bin /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
+	cp /system/etc/wifi/WCNSS_qcom_cfg.ini /data/misc/wifi/WCNSS_qcom_cfg.ini
+	chown -h system:wifi /data/misc/wifi/WCNSS_qcom_cfg.ini
+	chown -h system:wifi /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
+	chmod -h 660 /data/misc/wifi/WCNSS_qcom_cfg.ini
+	chmod -h 660 /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
 
-		rm /data/misc/wifi/WCNSS_qcom_cfg.ini
-		rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
+	# The property below is used in Qcom SDK for softap to determine
+	# the wifi driver config file
+	setprop wlan.driver.config /data/misc/wifi/WCNSS_qcom_cfg.ini
 
-		cp /system/etc/wifi/WCNSS_qcom_wlan_nv.bin /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
-		cp /system/etc/wifi/WCNSS_qcom_cfg.ini /data/misc/wifi/WCNSS_qcom_cfg.ini
+	# Trigger WCNSS platform driver
+	# We need to trigger WCNSS platform driver, WCNSS driver
+	# will export a file which we must touch so that the
+	# driver knows that userspace is ready to handle firmware
+	# download requests.
 
-		chown -h system:wifi /data/misc/wifi/WCNSS_qcom_cfg.ini
-		chown -h system:wifi /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
-
-		chmod -h 660 /data/misc/wifi/WCNSS_qcom_cfg.ini
-		chmod -h 660 /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
-
-		# The property below is used in Qcom SDK for softap to determine
-		# the wifi driver config file
-		setprop wlan.driver.config /data/misc/wifi/WCNSS_qcom_cfg.ini
-
-		# Trigger WCNSS platform driver
-		# We need to trigger WCNSS platform driver, WCNSS driver
-		# will export a file which we must touch so that the
-		# driver knows that userspace is ready to handle firmware
-		# download requests.
-
-		# See if an appropriately named device file is present
-		wcnssnode=`ls /dev/wcnss*`
-		case "$wcnssnode" in
-		*wcnss*)
-			# Before triggering wcnss, let it know that
-			# caldata is available at userspace.
-			if [ -e /data/misc/wifi/WCNSS_qcom_wlan_cal.bin ]; then
-				calparm=`ls /sys/module/wcnsscore/parameters/has_calibrated_data`
-				if [ -e $calparm ] && [ ! -e /data/misc/wifi/WCN_FACTORY ]; then
-					echo 1 > $calparm
-				fi
+	# See if an appropriately named device file is present
+	wcnssnode=`ls /dev/wcnss*`
+	case "$wcnssnode" in
+	*wcnss*)
+		# Before triggering wcnss, let it know that
+		# caldata is available at userspace.
+		if [ -e /data/misc/wifi/WCNSS_qcom_wlan_cal.bin ]; then
+			calparm=`ls /sys/module/wcnsscore/parameters/has_calibrated_data`
+			if [ -e $calparm ] && [ ! -e /data/misc/wifi/WCN_FACTORY ]; then
+				echo 1 > $calparm
 			fi
-			# There is a device file.  Write to the file
-			# so that the driver knows userspace is
-			# available for firmware download requests
-			echo 1 > $wcnssnode
-			;;
-		*)
-			# There is not a device file present, so
-			# the driver must not be available
-			echo "No WCNSS device node detected"
-			;;
-		esac
-
-		# Plumb down the device serial number
-		if [ -f /sys/devices/*wcnss-wlan/serial_number ]; then
-			cd /sys/devices/*wcnss-wlan
-			echo $serialno > serial_number
-			cd /
-		elif [ -f /sys/devices/platform/wcnss_wlan.0/serial_number ]; then
-			echo $serialno > /sys/devices/platform/wcnss_wlan.0/serial_number
 		fi
-		;;
+		# There is a device file.  Write to the file
+		# so that the driver knows userspace is
+		# available for firmware download requests
+		echo 1 > $wcnssnode;;
+	*)
+		# There is not a device file present, so
+		# the driver must not be available
+		echo "No WCNSS device node detected";;
 	esac
 
-	touch /data/misc/wifi/wifi_fixup
-
-	mount -o ro,remount,barrier=1 /system
-fi
+	# Plumb down the device serial number
+	serialno=`getprop ro.serialno`
+	if [ -f /sys/devices/*wcnss-wlan/serial_number ]; then
+		cd /sys/devices/*wcnss-wlan
+		echo $serialno > serial_number
+		cd /
+	elif [ -f /sys/devices/platform/wcnss_wlan.0/serial_number ]; then
+		echo $serialno > /sys/devices/platform/wcnss_wlan.0/serial_number
+	fi;;
+esac
