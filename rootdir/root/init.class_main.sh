f3089040a9ad0f4cfa3d90d394f2d2b634129354
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-03-18 08:44:04 -0300

    init: Update copyright
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- rootdir/root/init.class_main.sh
+++ rootdir/root/init.class_main.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
 # Copyright (c) 2013, The Linux Foundation. All rights reserved.
+# Copyright (C) 2016, TeamVeeSS. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
