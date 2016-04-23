2ec5df49f026b377322d84badb5decdaec99abf5
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-04-23 00:32:30 -0300

    Android.mk: Firmware symlinks
    TEST
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- Android.mk
+++ Android.mk
@@ -19,3 +19,21 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),falconss)
 	include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
+
+include $(CLEAR_VARS)
+
+# Create firmware links
+FIRMWARE_IMAGES := \
+    modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 modem.b08 modem.b15 modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 \
+    modem.b21 modem.b22 modem.b25 modem.b26 modem.b27 modem.mdt \
+    wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b06 wcnss.b07 wcnss.b08 wcnss.b09 wcnss.mdt
+
+FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_IMAGES)))
+$(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_SYMLINKS)
+
