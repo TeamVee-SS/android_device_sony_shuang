fbb16bc1c244a69e50076f04a6255959d966ff4b
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-04-24 21:05:11 -0300

    rootdir: root: More clean

--- rootdir/root/init.qcom.class_main.sh
+++ rootdir/root/init.qcom.class_main.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 # Copyright (c) 2013, The Linux Foundation. All rights reserved.
-# Copyright (C) 2016, TeamVeeSS. All rights reserved.
+# Copyright (c) 2016, TeamVee-SS. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -36,4 +36,3 @@ fi
 # Set secondary things
 setprop ro.build.description "$(getprop ro.build.product)-$(getprop ro.build.type) $(getprop ro.build.version.release) $(getprop ro.build.id) $(getprop ro.build.version.incremental) $(getprop ro.build.tags)"
 setprop ro.build.fingerprint "$(getprop ro.product.manufacturer)/$(getprop ro.build.product)/$(getprop ro.build.product):$(getprop ro.build.version.release)/$(getprop ro.build.id):$(getprop ro.build.type)/$(getprop ro.build.tags)"
-
--- rootdir/root/init.qcom.sh
+++ rootdir/root/init.qcom.sh
@@ -28,53 +28,36 @@
 #
 
 for file in /proc/sys/net/ipv6/conf/*
-	do echo 0 > $file/accept_ra_defrtr
+do
+	echo 0 > $file/accept_ra_defrtr
 done
 echo 1 > /proc/sys/net/ipv6/conf/default/accept_ra_defrtr
 
-if [ -c /dev/msm_dsps -o -c /dev/sensors ]; then
-	chmod -h 775 /persist/sensors
-	chmod -h 664 /persist/sensors/sensors_settings
-	chown -h system.root /persist/sensors/sensors_settings
-	mkdir -p /data/misc/sensors
-	chmod -h 775 /data/misc/sensors
-	start sensors
-fi
+chown -h root.system /sys/module/qpnp_charger/parameters/*
+chown -h root.system /sys/class/power_supply/battery/input_current_max
+chown -h root.system /sys/class/power_supply/battery/input_current_trim
+chown -h root.system /sys/class/power_supply/battery/input_current_settled
+chown -h root.system /sys/class/power_supply/battery/voltage_min
+chmod -h 0664 /sys/class/power_supply/battery/input_current_max
+chmod -h 0664 /sys/class/power_supply/battery/input_current_trim
+chmod -h 0664 /sys/class/power_supply/battery/input_current_settled
+chmod -h 0664 /sys/class/power_supply/battery/voltage_min
+chmod -h 0664 /sys/module/qpnp_charger/parameters/charger_monitor
+start charger_monitor
 
-if ls /sys/module/qpnp_charger/parameters/charger_monitor; then
-	chown -h root.system /sys/module/qpnp_charger/parameters/*
-	chown -h root.system /sys/class/power_supply/battery/input_current_max
-	chown -h root.system /sys/class/power_supply/battery/input_current_trim
-	chown -h root.system /sys/class/power_supply/battery/input_current_settled
-	chown -h root.system /sys/class/power_supply/battery/voltage_min
-	chmod -h 0664 /sys/class/power_supply/battery/input_current_max
-	chmod -h 0664 /sys/class/power_supply/battery/input_current_trim
-	chmod -h 0664 /sys/class/power_supply/battery/input_current_settled
-	chmod -h 0664 /sys/class/power_supply/battery/voltage_min
-	chmod -h 0664 /sys/module/qpnp_charger/parameters/charger_monitor
-	start charger_monitor
-fi
+echo 4 > /sys/module/lpm_levels/enable_low_power/l2
+echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu1/power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu0/standalone_power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu1/standalone_power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu0/standalone_power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu1/standalone_power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/idle_enabled
+# Start mpdecision
+start mpdecision
 
-case `getprop ro.board.platform` in
-    "msm8610")
-        echo 4 > /sys/module/lpm_levels/enable_low_power/l2
-        echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/suspend_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu1/power_collapse/suspend_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu0/standalone_power_collapse/suspend_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu1/standalone_power_collapse/suspend_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu0/standalone_power_collapse/idle_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu1/standalone_power_collapse/idle_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/idle_enabled
-        # Start mpdecision
-        start mpdecision
-    ;;
-esac
+chown -h system /sys/devices/platform/rs300000a7.65536/force_sync
+chown -h system /sys/devices/platform/rs300000a7.65536/sync_sts
+chown -h system /sys/devices/platform/rs300100a7.65536/force_sync
+chown -h system /sys/devices/platform/rs300100a7.65536/sync_sts
 
-case `getprop ro.boot.emmc` in
-    "true")
-        chown -h system /sys/devices/platform/rs300000a7.65536/force_sync
-        chown -h system /sys/devices/platform/rs300000a7.65536/sync_sts
-        chown -h system /sys/devices/platform/rs300100a7.65536/force_sync
-        chown -h system /sys/devices/platform/rs300100a7.65536/sync_sts
-    ;;
-esac
