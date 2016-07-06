50ab3fe8cb458f15873f6106b841cc6cd763a127
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-07-05 22:05:31 -0300

    falconss: Add backlight paths
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_SPECIFIC_HEADER_PATH += device/sony/falconss/include
+
 # inherit from the proprietary version
 -include vendor/sony/falconss/BoardConfigVendor.mk
 
