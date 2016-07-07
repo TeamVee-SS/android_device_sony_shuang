c11b69ee240480977e4d4b73a57268de38485729
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-07-07 11:47:21 -0300

    bt: Update based on XperiaZ1/Honami
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,9 +19,16 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME	"Xperia E1"
-#define BTA_DISABLE_DELAY	1000
-#define BLUETOOTH_QCOM_SW	TRUE
-#define BLUETOOTH_QCOM_LE_INTL_SCAN	TRUE
-#define BTC_INCLUDED	TRUE
+#define BTA_DISABLE_DELAY	1000 /* in milliseconds */
+
+// Enables Interleave scan
+#define BTA_HOST_INTERLEAVE_SEARCH	TRUE
+
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT	TRUE
+#define MAX_L2CAP_CHANNELS		14
+
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD	TRUE
 
 #endif
