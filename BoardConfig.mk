67eeb04884dc01cfdedf14026754a1949a8355d2
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-07-31 21:27:49 -0300

    falconss: Suppress User Space Unhandled Page fault logs in dmesg
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_KERNEL_CONFIG := cyanogenmod_falconss_defconfig
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 maxcpus=2 msm_rtb.filter=0x3F ehci-hcd.park=3 msm_rtb.enable=0 lpj=192598 dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=23 maxcpus=2 msm_rtb.filter=0x3F ehci-hcd.park=3 msm_rtb.enable=0 lpj=192598 dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
 
 # Lights HAL
