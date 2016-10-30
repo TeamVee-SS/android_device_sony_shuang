59a8374b0f48781b834928d2637ef714a6781f91
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-10-30 17:54:43 -0200

    boot: init.sh: Fix alot of minor issues
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- boot/init.sh
+++ boot/init.sh
@@ -1,19 +1,14 @@
 #!/sbin/busybox sh
 set +x
-_PATH=${PATH}
-export PATH="/sbin"
-
-busybox cd /
-busybox date >> boot.txt
-exec >> boot.txt 2>&1
-busybox rm /init
+exec >> /boot.txt 2>&1
+/sbin/busybox rm /init
 
 triggerled() {
 # Use this <http://www.nameacolor.com/Color%20numbers.htm>
-busybox echo ${1} > /sys/class/leds/red/brightness
-busybox echo ${2} > /sys/class/leds/green/brightness
-busybox echo ${3} > /sys/class/leds/notification/brightness
-busybox echo ${4} > /sys/class/leds/lm3533-light-sns/rgb_brightness
+/sbin/busybox echo ${1} > /sys/class/leds/red/brightness
+/sbin/busybox echo ${2} > /sys/class/leds/green/brightness
+/sbin/busybox echo ${3} > /sys/class/leds/notification/brightness
+/sbin/busybox echo ${4} > /sys/class/leds/lm3533-light-sns/rgb_brightness
 }
 
 # include device specific vars
@@ -23,62 +18,60 @@ export BOOTREC_FOTA_NODE="/dev/block/mmcblk0p16 b 179 16"
 export BOOTREC_FOTA="/dev/block/mmcblk0p16"
 
 # create directories
-busybox mkdir -m 755 -p /dev/block
-busybox mkdir -m 755 -p /dev/input
-busybox mkdir -m 555 -p /proc
-busybox mkdir -m 755 -p /sys
+/sbin/busybox mkdir -m 755 -p /dev/block
+/sbin/busybox mkdir -m 755 -p /dev/input
+/sbin/busybox mkdir -m 555 -p /proc
+/sbin/busybox mkdir -m 755 -p /sys
 
 # create device nodes
-busybox mknod -m 600 /dev/block/mmcblk0 b 179 0
-busybox mknod -m 600 ${BOOTREC_EVENT_NODE}
-busybox mknod -m 600 ${BOOTREC_FOTA_NODE}
-busybox mknod -m 666 /dev/null c 1 3
+/sbin/busybox mknod -m 600 /dev/block/mmcblk0 b 179 0
+/sbin/busybox mknod -m 600 ${BOOTREC_EVENT_NODE}
+/sbin/busybox mknod -m 600 ${BOOTREC_FOTA_NODE}
+/sbin/busybox mknod -m 666 /dev/null c 1 3
 
 # mount filesystems
-busybox mount -t proc proc /proc
-busybox mount -t sysfs sysfs /sys
+/sbin/busybox mount -t proc proc /proc
+/sbin/busybox mount -t sysfs sysfs /sys
 
 # trigger ON green LED
 triggerled 0 255 0 65280
 
 # trigger vibration
-busybox echo 100 > /sys/class/timed_output/vibrator/enable
+/sbin/busybox echo 100 > /sys/class/timed_output/vibrator/enable
 
 # keycheck
-busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
-busybox sleep 3
+/sbin/busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
+/sbin/busybox sleep 3
 
 # boot decision
-if [ -s /dev/keycheck ] || busybox grep -q warmboot=0x77665502 /proc/cmdline
+if [ -s /dev/keycheck ] || /sbin/busybox grep -q warmboot=0x77665502 /proc/cmdline
 then
 	# trigger ON cyan LED for recovery
 	triggerled 0 255 255 65535
 
 	# extract recovery ramdisk from fota
-	extract_elf_ramdisk -i ${BOOTREC_FOTA} -o /sbin/ramdisk-recovery.cpio -t / -c
+	/sbin/extract_elf_ramdisk -i ${BOOTREC_FOTA} -o /sbin/ramdisk-recovery.cpio -t / -c
 
 	# recovery ramdisk
 	load_image="/sbin/ramdisk-recovery.cpio"
-	busybox echo "RECOVERY BOOT" >> boot.txt
+	/sbin/busybox echo "RECOVERY BOOT"
 else
 	# android ramdisk
 	load_image="/sbin/ramdisk.cpio"
-	busybox echo "ANDROID BOOT" >> boot.txt
+	/sbin/busybox echo "ANDROID BOOT"
 fi
 
 # kill the keycheck process
-busybox pkill -f "busybox cat ${BOOTREC_EVENT}"
+/sbin/busybox pkill -f "busybox cat ${BOOTREC_EVENT}"
 
 # trigger OFF LED
 triggerled 0 0 0 0
 
 # unpack the ramdisk image
-busybox cpio -i < ${load_image}
+/sbin/busybox cpio -i < ${load_image}
 
-busybox umount /proc
-busybox umount /sys
+/sbin/busybox umount /proc
+/sbin/busybox umount /sys
+/sbin/busybox rm -fr /dev/*
 
-busybox rm -fr /dev/*
-busybox date >> boot.txt
-export PATH=${_PATH}
 exec /init
