f3a01f3df56c83dcfd8ccbd44279b8ea21ec7c83
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-10-31 23:19:55 -0200

    cmhw: Backport to CyanogenMod 11.0
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -16,9 +16,11 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.internal.util.FileUtils;
+import org.cyanogenmod.hardware.util.FileUtils;
 
 import android.util.Log;
+
+import java.io.File;
 
 public class VibratorHW {
 
@@ -30,10 +32,7 @@ public class VibratorHW {
     private static final String MIN_PATH = "/sys/class/timed_output/vibrator/vtg_min";
 
     public static boolean isSupported() {
-        return FileUtils.isFileWritable(LEVEL_PATH) &&
-                FileUtils.isFileReadable(DEFAULT_PATH) &&
-                FileUtils.isFileReadable(MAX_PATH) &&
-                FileUtils.isFileReadable(MIN_PATH);
+        return new File(LEVEL_PATH).exists();
     }
 
     public static int getMaxIntensity() {
