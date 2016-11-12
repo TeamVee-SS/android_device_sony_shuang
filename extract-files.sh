2ef4a386f5229e4d12f473e5942ffbf65c1bc692
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-11-12 18:12:13 -0200

    extract-files.sh: Slight update
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -3,30 +3,66 @@ DEVICE=falconss
 VENDOR=sony
 BASE=../../../vendor/${VENDOR}/${DEVICE}/proprietary
 
-for FILE in `grep -v ^# ../${DEVICE}/proprietary-files.txt | grep -v ^$ | sort`
+rm -rf ${BASE}/*
+
+if [ -d ../system ]; then
+  SRC="../"
+fi
+
+if [ "$#" == "0" ]; then
+  if [ -d ../system ]; then
+    SRC=".."
+  else
+    SRC="adb"
+  fi
+else
+  if [ "$#" == "1" ]; then
+    SRC="$1"
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs"
+    echo "will be extracted from the device using adb pull."
+    exit 1
+  fi
+fi
+
+for FILE in $(grep -v ^# ../${DEVICE}/proprietary-files.txt | grep -v ^$ | sort)
 do
   # Split the file from the destination (format is "file[:destination]")
   OLDIFS=${IFS} IFS=":" PARSING_ARRAY=(${FILE}) IFS=${OLDIFS}
   FILE=${PARSING_ARRAY[0]}
   DEST=${PARSING_ARRAY[1]}
   if [[ "${FILE}" =~ ^-.* ]]; then
-    FILE=`echo ${FILE} | sed s/^-//`
+    FILE=$(echo ${FILE} | sed s/^-//)
   fi
   if [ -z "${DEST}" ]; then
     DEST=${FILE}
   fi
-  DIR=`dirname ${DEST}`
+  DIR=$(dirname ${DEST})
+
   if [ ! -d ${BASE}/${DIR} ]; then
     mkdir -p ${BASE}/${DIR}
   fi
-
-  cp ../system/${FILE} ${BASE}/${DEST}
-  if [ "$?" != "0" ]; then
+  if [ "${SRC}" == "adb" ]; then
     adb pull /system/${FILE} ${BASE}/${DEST}
-  fi
-  if [ "$?" != "0" ]; then
-    adb pull /system/${DEST} ${BASE}/${DEST}
+    if [ "$?" != "0" ]; then
+      adb pull /system/${DEST} ${BASE}/${DEST}
+    fi
+  else
+    cp ${SRC}/system/${FILE} ${BASE}/${DEST}
+    if [ "$?" != "0" ]; then
+      cp ${SRC}/system/${DEST} ${BASE}/${DEST}
+      if [ "$?" != "0" ]; then
+        cp ${SRC}/${FILE} ${BASE}/${DEST}
+        if [ "$?" != "0" ]; then
+          cp ${SRC}/${DEST} ${BASE}/${DEST}
+        fi
+      fi
+    fi
   fi
 done
 
-. setup-makefiles.sh
+. ../${DEVICE}/setup-makefiles.sh
