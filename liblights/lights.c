7c555685df98c918922174c73c8c8522995c4c43
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-01 23:13:27 -0200

    liblights: falconss: Remove button backlight
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- liblights/lights.c
+++ liblights/lights.c
@@ -118,19 +118,6 @@ static int set_light_backlight (struct light_device_t *dev, struct light_state_t
 	return err;
 }
 
-static int set_light_buttons (struct light_device_t *dev, struct light_state_t const* state) {
-	size_t i = 0;
-	int err = 0;
-	int on = is_lit(state);
-	pthread_mutex_lock(&g_lock);
-
-	err = write_int (BUTTON_BACKLIGHT_FILE, on ? rgb_to_brightness(state) : 0);
-
-	pthread_mutex_unlock(&g_lock);
-
-	return 0;
-}
-
 static void set_shared_light_locked (struct light_device_t *dev, struct light_state_t *state) {
 	int r, g, b;
 	int err = 0;
@@ -201,9 +188,6 @@ static int open_lights (const struct hw_module_t* module, char const* name,
 
 	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name)) {
 		set_light = set_light_backlight;
-	}
-	else if (0 == strcmp(LIGHT_ID_BUTTONS, name)) {
-		set_light = set_light_buttons;
 	}
 	else if (0 == strcmp(LIGHT_ID_BATTERY, name)) {
 		set_light = set_light_battery;
