2f0b9ab6d2ee2427f1d04c5e8d4fc79a1a1f52fd
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-03 16:59:31 -0200

    falconss: Remove custom healthd
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,9 +144,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 # CMHW
 BOARD_HARDWARE_CLASS := device/sony/falconss/cmhw
 
-# Override healthd HAL
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.qcom
-
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
