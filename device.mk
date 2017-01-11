d2a6aa93a5a7f043d93c4d0e0d5a52dfe3427d9b
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-11 17:05:49 -0200

    falconss: Remove wait4tad_static
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- device.mk
+++ device.mk
@@ -33,7 +33,6 @@ PRODUCT_COPY_FILES += \
     device/sony/falconss/rootdir/root/init.qcom.class_main.sh:root/init.qcom.class_main.sh \
     device/sony/falconss/rootdir/root/init.qcom.early_boot.sh:root/init.qcom.early_boot.sh \
     device/sony/falconss/rootdir/root/init.qcom.usb.rc:root/init.qcom.usb.rc \
-    device/sony/falconss/rootdir/root/sbin/wait4tad_static:root/sbin/wait4tad_static \
     device/sony/falconss/rootdir/root/sbin/tad_static:root/sbin/tad_static \
     device/sony/falconss/rootdir/root/ueventd.qcom.rc:root/ueventd.qcom.rc
 
