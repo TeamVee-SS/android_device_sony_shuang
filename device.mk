dd760be481acf1c027f9c1b51c8c43ce5d7b6e2e
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-29 16:01:23 -0200

    falconss: Remove duplicate and unused
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- device.mk
+++ device.mk
@@ -203,9 +203,6 @@ PRODUCT_PACKAGES += \
     timekeep \
     TimeKeep
 
-PRODUCT_PACKAGES += \
-    com.google.widevine.software.drm
-
 PRODUCT_GMS_CLIENTID_BASE ?= android-sonymobile
 
 # For userdebug builds
