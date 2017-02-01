c8bf708a59128441eb9e73f248b0cc37f9885dec
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-02-01 11:06:41 -0200

    falconss: Optimize ALL!
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,6 @@ ifeq ($(HOST_OS),linux)
   ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-      DONT_DEXPREOPT_PREBUILTS := true
     endif
   endif
 endif
