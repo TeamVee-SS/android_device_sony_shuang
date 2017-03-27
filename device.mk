3efcf374ccc3a3d3ed5465e495d0e57f70f33e82
tetsuo55 <tetsuo55@users.noreply.github.com>
2017-03-27 19:22:23 +0200

    shuang: device.mk: Configure libhwui
    All values modified for shuang's 800x480 screen.
    
    Change-Id: Iff454a65b873d5939fb1043db6730468f82d0e9d
    source: https://source.android.com/devices/tech/config/renderer.html

--- device.mk
+++ device.mk
@@ -215,6 +215,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version="196608"
 
+# libhwui
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=9 \
+    ro.hwui.layer_cache_size=6 \
+    ro.hwui.r_buffer_cache_size=0.75 \
+    ro.hwui.path_cache_size=1.5 \
+    ro.hwui.gradient_cache_size=0.5 \
+    ro.hwui.drop_shadow_cache_size=0.75 \
+    ro.hwui.texture_cache_flushrate=0.5 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=256 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=512
+
 # Debug
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type="dyn" \
