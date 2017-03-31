f3ae4d594572ae17d11f5ca963f5c2be6238d3b0
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    bluetooth: Add libbt-vendor to products
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- device.mk
+++ device.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
     camera.qcom \
     libcamera_parameters_shim
 
+# Bluetooth packages
+PRODUCT_PACKAGES += \
+    libbt-vendor
+
 # Bluetooth HIDL interfaces
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl
