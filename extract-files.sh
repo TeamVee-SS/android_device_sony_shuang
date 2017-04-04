600be2b14729f8f7fd97198a736be44ddc9d208e
Rashed Abdel-Tawab <rashed@linux.com>
2017-04-04 03:53:51 -0400

    proprietary: Upstream updates
    proprietary: Add support for new global extract_files features
    
    * Add support for specifying blob sections
    * Default to bypassing vendor cleaning and add an option to force it
    * Add support for paths that do not contain "system/"
    
    Change-Id: I2d5397374dc6e3e29299c69da6beca73d7857a2c
    
    extract-files: Default to sanitize vendor folder during extraction
    
     * This was always the default behaviour and doesn't require
       any additional argument in order to make use of pinning feature
       (pinned blobs are only considered when vendor folder is removed)
    
    Change-Id: I34ff7678bd6800a2c593e9fe128cc151c59967da

--- extract-files.sh
+++ extract-files.sh
@@ -18,10 +18,11 @@
 
 set -e
 
-DEVICE=shuang
-VENDOR=sony
+# Required!
+export DEVICE=shuang
+export VENDOR=sony
 
-# Load extractutils and do some sanity checks
+# Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
@@ -34,25 +35,33 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --path )           shift
+                                SRC=$1
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
 fi
 
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+# Initialize the helper for device
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC"
+# Sony/Device specific blobs
+extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
+# Generate vendor makefiles
 "$MY_DIR"/setup-makefiles.sh
