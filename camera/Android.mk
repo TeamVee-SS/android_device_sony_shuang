b1e62f9035ad8b8b335282f2d3f9fb7ac5f5a7e6
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-04-22 16:47:43 -0300

    Revert "shuang: Improve Camera HAL wrapper for SensorService init"
    This reverts commit 4b18a2caa137c9ed32dd3a70db4865d25b96dcc4.

--- camera/Android.mk
+++ camera/Android.mk
@@ -10,10 +10,11 @@ LOCAL_C_INCLUDES := \
     system/media/camera/include
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils
+    libhardware liblog libcamera_client libutils
 
-LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.qcom
+
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
