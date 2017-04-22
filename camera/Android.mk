ffa6404e3e2cc14c0b959151bbed10733fe9571f
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-04-22 16:42:37 -0300

    Revert "shuang: Add shim library for camera"
    This reverts commit 923786a57abf3c7b96e95f502b86fe7bfb831895.

--- camera/Android.mk
+++ camera/Android.mk
@@ -19,15 +19,3 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 endif
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    hw_camera.c
-
-LOCAL_SHARED_LIBRARIES := \
-    liblog libcutils libgui libbinder libutils
-
-LOCAL_MODULE := libcam
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
-include $(BUILD_SHARED_LIBRARY)
