171eefa9dedcf9b1a6d497c5deab1d447739908f
Tetsuo Osaka <tetsugit@gmail.com>
2017-04-28 08:28:55 +0200

    shuang: move vendor bins to /vendor
    follow qcom-common locations for qcom files
    follow sony-common locations for sony files
    this allows for a small reduction on sepolicy
    
    Change-Id: I371aaa62b1ee51e1e35f754edcb295ed07494bf5

--- device.mk
+++ device.mk
@@ -32,7 +32,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/root/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/root/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/root/init.qcom.usb.rc:root/init.qcom.usb.rc \
-    $(LOCAL_PATH)/rootdir/root/sbin/tad_static:root/sbin/tad_static \
     $(LOCAL_PATH)/rootdir/root/ueventd.qcom.rc:root/ueventd.qcom.rc
 
 # Config Files
