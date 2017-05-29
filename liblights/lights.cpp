17cbd1e1a84b9d2a99c1464a9e39a4969fe86f0a
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-05-29 00:07:57 -0300

    liblights: Convert to C++
    Change-Id: I3651272f25dd55b260ec62f0b7b2ff841bcd63c4
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- liblights/lights.cpp
+++ liblights/lights.cpp
@@ -184,11 +184,7 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 	}
 
 	pthread_once(&g_init, init_globals);
-
-	struct light_device_t *dev = malloc(sizeof(struct light_device_t));
-	if(!dev)
-		return -ENOMEM;
-
+	struct light_device_t* dev = (struct light_device_t*) malloc(sizeof(struct light_device_t));
 	memset(dev, 0, sizeof(*dev));
 
 	dev->common.tag = HARDWARE_DEVICE_TAG;
