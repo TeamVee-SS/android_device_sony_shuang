f6287fdb9c639885f6ad2d9248cd1edd9c9f8684
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-09-10 20:27:15 -0300

    shuang: Rebrand to CM -> Lineage charger
    Change-Id: I0e66bc9c6ff5c27588f09a054222933561c9e3e8
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,7 @@ TARGET_OTA_ASSERT_DEVICE := D2004,D2005,D2104,D2105,D2114,falconss,shuang
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
-WITH_CM_CHARGER := true
+WITH_LINEAGE_CHARGER := true
 BLUE_LED_PATH := /sys/class/leds/notification/brightness
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
